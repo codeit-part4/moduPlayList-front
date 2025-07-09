5f176a27112b978e4a2b2049bce1a2239eecc94b
hoosa <janghoosa@gmail.com>
2025-07-09 19:07:32 +0900

    Feat: 사이드바 링크로 수정

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom'
 
 const SidebarContainer = styled.div`
         width: 220px;
@@ -7,11 +8,16 @@ const SidebarContainer = styled.div`
         border-right: 1px solid #ddd;
 `;
 
-const MenuItem = styled.div`
+const MenuItem = styled(Link)`
         margin-bottom: 16px;
         font-size: 14px;
         cursor: pointer;
-
+        text-decoration: none;
+        color: inherit;
+        display: flex;
+        align-items: center;
+        gap: 8px;
+        
         &:hover {
                 color: #866bff;
         }
@@ -20,10 +26,10 @@ const MenuItem = styled.div`
 const Sidebar: React.FC = () => (
     <SidebarContainer>
         <h2>모두의 플리</h2>
-        <MenuItem>⭐ 콘텐츠 같이 보기</MenuItem>
-        <MenuItem>⭐ 플레이 리스트</MenuItem>
-        <MenuItem>⭐ 프로필</MenuItem>
-        <MenuItem>⭐ 사용자 관리</MenuItem>
+        <MenuItem to={'/home'}>⭐ 콘텐츠 같이 보기</MenuItem>
+        <MenuItem to={'/playList'}>⭐ 플레이 리스트</MenuItem>
+        <MenuItem to={"/profile"}>⭐ 프로필</MenuItem>
+        <MenuItem to={"/profile"}>⭐ 사용자 관리</MenuItem>
     </SidebarContainer>
 );
 
