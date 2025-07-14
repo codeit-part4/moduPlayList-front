f6e4bd950b97e80fdc21a9189b5f17c32e12928f
hoosa <janghoosa@gmail.com>
2025-07-14 11:04:43 +0900

    fix: 빌드 에러 수정

--- src/pages/ContentReviewsPage.tsx
+++ src/pages/ContentReviewsPage.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 import ContentDetailInfo from '../components/ContentDetailInfo';
 import ReviewList from '../components/ReviewList';
+import { dummyContents } from '../type/contents.ts';
 
 const PageLayout = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 32px;
+    display: flex;
+    flex-direction: column;
+    gap: 32px;
 `;
 
 const ContentReviewsPage: React.FC = () => {
+  const content = dummyContents[0];
   return (
     <PageLayout>
-      <ContentDetailInfo />
+      <ContentDetailInfo content={content} />
       <ReviewList />
     </PageLayout>
   );
