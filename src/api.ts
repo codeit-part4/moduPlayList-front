9d49f7ceeec5e47ae9c4728e7554a2ff9e94539a
Haram0111 <5gkfka5@naver.com>
2025-07-24 10:26:43 +0900

    Fix : url 수정

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
 export const 
-API_BASE_URL = "http://mople-alb-1855363092.ap-northeast-2.elb.amazonaws.com/";
+API_BASE_URL = "http://mople-alb-1855363092.ap-northeast-2.elb.amazonaws.com";
 
 //localhost:8080
